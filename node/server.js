641e3cbe013dc72cbf7443f9cca9251e5dc2f130
Now <mmkr4934@naver.com>
2022-09-07 10:43:31 +0900

    node 폴더 생성 및 add commit

--- node/server.js
+++ node/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(5555, function () {
   console.log("i am the king!");
@@ -13,6 +15,12 @@ app.get("/", function (요청, 응답) {
   응답.sendFile(__dirname + "/index.html");
 });
 
-app.get("/pet", function (요청, 응답) {
-  응답.send("펫 용품점입니다.");
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
+});
+
+app.post("/add", function (요청, 응답) {
+  응답.send("전송완료");
+  console.log(요청.body.title);
+  console.log(요청.body.sub);
 });
