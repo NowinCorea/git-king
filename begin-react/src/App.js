119ec85a20180759475c4149c13126043071127a
Now <mmkr4934@naver.com>
2022-10-16 17:36:23 +0900

    todolist commit

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import Hello from './Hello';
 import './App.css';
-import Wrapper from './Wrapper';
+import Hello from './component/hello.js';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'coral',
-    fontSize: 24,
-    padding: '1rem',
-  };
   return (
-    <Wrapper>
-      <Hello color="cornflowerblue" name="KingChan" />
-      <Hello color="pink" />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </Wrapper>
+    <>
+      <Hello />
+      <Hello />
+      <Hello />
+    </>
   );
 }
 
